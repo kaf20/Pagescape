651efd472fdf4364af3bcdad54098456770d8220
Lau Ka Fai <nelson.kf.lau@gmail.com>
2018-10-30 01:43:42 +0800

    Added calculator activity

--- app/src/main/java/cc/lockorder/ttcalc/MainActivity.kt
+++ app/src/main/java/cc/lockorder/ttcalc/MainActivity.kt
@@ -1,5 +1,7 @@
 package cc.lockorder.ttcalc
 
+import android.app.TaskStackBuilder
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
@@ -19,21 +21,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val exchangeRates = ExchangeRateService.fetch()
-        val recyclerView : RecyclerView = findViewById(R.id.rateListView)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = ExchangeRateAdapter(exchangeRates)
+        val onClickListener = View.OnClickListener {
+            TaskStackBuilder.create(this)
+                .addNextIntentWithParentStack(
+                    Intent(this, CalculatorActivity::class.java)
+                ).startActivities()
+        }
+        findViewById<RecyclerView>(R.id.rateListView).apply {
+            setHasFixedSize(true)
+            adapter = ExchangeRateAdapter(exchangeRates, onClickListener)
+        }
     }
 }
 
 object ExchangeRateService {
 
-    fun fetch() : List<ExchangeRate> {
-        // Delegate to NDK to fetch rate
-        return listOf(
+    fun fetch() : List<ExchangeRate> = listOf(
             ExchangeRate(Currency.getInstance("HKD"), BigDecimal.valueOf(7.85)),
             ExchangeRate(Currency.getInstance("GBP"), BigDecimal.valueOf(10.05))
         )
-    }
 }
 
 // ---------------------------------------------------------------------------------------------------
@@ -42,14 +48,13 @@ object ExchangeRateService {
 
 data class ExchangeRate(val currency: Currency, val rate: BigDecimal)
 
-class ExchangeRateAdapter(private val exchangeRates: List<ExchangeRate> = emptyList())
+class ExchangeRateAdapter(private val exchangeRates: List<ExchangeRate> = emptyList(),
+                          private val onClickListener: View.OnClickListener)
     : RecyclerView.Adapter<ExchangeRateAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
         val view : View = LayoutInflater.from(p0.context).inflate(R.layout.exchange_rate_item, p0, false)
-        view.calculateExchangeRateButton.setOnClickListener { b ->
-            TODO("Delegate to field component")
-        }
+        view.calculateExchangeRateButton.setOnClickListener(onClickListener)
         return ViewHolder(view)
     }
 
@@ -57,9 +62,7 @@ class ExchangeRateAdapter(private val exchangeRates: List<ExchangeRate> = emptyL
         p0.nameTextView.text = exchangeRates[p1].currency.currencyCode.plus("@").plus(exchangeRates[p1].rate)
     }
 
-    override fun getItemCount(): Int {
-        return exchangeRates.count()
-    }
+    override fun getItemCount(): Int = exchangeRates.count()
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var nameTextView: TextView = itemView.findViewById(R.id.exchangeRateTextView)
