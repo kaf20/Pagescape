0839ac59dbe787ca12cc44f067ee362502ea5232
Lau Ka Fai <nelson.kf.lau@gmail.com>
2018-11-07 16:28:08 +0800

    Apply Persistent Drawer

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux'
 import retrieveRatesReducer from './retrieveRatesReducer'
+import toggleDrawerOpenReducer from './toggleDrawerOpenReducer'
 
 export default combineReducers({
-    retrieveRatesReducer
+    retrieveRatesReducer,
+    toggleDrawerOpenReducer,
 })
