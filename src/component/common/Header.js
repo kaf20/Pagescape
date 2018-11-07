f69f8fb10f1805f8b9adf5f3fac5198b0a294cb8
Lau Ka Fai <nelson.kf.lau@gmail.com>
2018-11-07 11:00:29 +0800

    Hide drawer button

--- src/component/common/Header.js
+++ src/component/common/Header.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import {withStyles} from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import React from 'react'
+import PropTypes from 'prop-types'
+import {withStyles} from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
+import Drawer from '@material-ui/core/Drawer'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
 
 const drawerWidth = 240
 const styles = theme => ({
@@ -63,26 +61,11 @@ const Header = (props) => {
     const handleClose = () => {
         console.debug("TODO") // TODO
     }
-    const handleDrawerOpen = () => {
-        console.debug("TODO") // TODO
-    };
-
     return (
         <div className={classes.root}>
             <CssBaseline />
             <AppBar position="fixed" className={classes.appBar}>
                 <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="Open drawer"
-                        onClick={handleDrawerOpen}
-                        className={classNames(
-                            classes.menuButton,
-                            state.open && classes.menuButtonHidden,
-                        )}
-                    >
-                        <MenuIcon />
-                    </IconButton>
                     <Typography variant="h6" color="inherit" className={classes.grow} align='center'>
                         <img src='https://hk.ttrate.com/img/logos/cur_cny.png?v=1536485358' height="32" width="42"/>
                         {title}
@@ -155,4 +138,4 @@ Header.propTypes = {
     title: PropTypes.string.isRequired
 }
 
-export default withStyles(styles)(Header);
+export default withStyles(styles)(Header)
