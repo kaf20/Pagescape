f9b6e0f421f04564c8546130845303de3c9af037
Lau Ka Fai <nelson.kf.lau@gmail.com>
2018-11-11 01:31:12 +0800

    use plain tentatively

--- src/AxiosConfig.js
+++ src/AxiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://web01.lockorder.cc:8443/ttcalc-web'
+axios.defaults.baseURL = 'http://www.pric.me:6080/ttcalc-web'
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
 axios.defaults.headers.post['Content-Type'] = 'application/json'
