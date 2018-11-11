b8637a8ea0ca813d66f596a5d90fb182bba74172
Lau Ka Fai <nelson.kf.lau@gmail.com>
2018-11-11 18:46:40 +0800

    remote www

--- src/AxiosConfig.js
+++ src/AxiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://www.pric.me/ttcalc-web'
+axios.defaults.baseURL = 'https://pric.me/ttcalc-web'
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
 axios.defaults.headers.post['Content-Type'] = 'application/json'
