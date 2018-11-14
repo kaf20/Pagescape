dd3e1db415c058f6c6cfe04da81275f3a583fba2
Lau Ka Fai <nelson.kf.lau@gmail.com>
2018-11-14 23:47:22 +0800

    refresh, show snack bar after successfully adding product

--- src/sagas/addProductDialogSaga.js
+++ src/sagas/addProductDialogSaga.js
@@ -5,21 +5,26 @@ function* doRetrieve(action) {
     try {
         const api = () =>
             axios.post('/product', {
-                name: 'abc',
-                price: 9.8,
-                place: 'place',
-                description: 'description',
-                imageUrl: 'imageUrl',
+                name: action.payload.name,
+                price: action.payload.price,
+                place: action.payload.place,
+                description: action.payload.description,
+                imageUrl: action.payload.imageUrl,
             })
         const result = yield call(api)
         yield put({type: 'RDX_TOGGLE_PRODUCT_DIALOG_OPEN', payload: {
             isAddProductDialogOpen: action.payload.isAddProductDialogOpen
         }})
+        yield put({type: 'SGA_RETRIEVE_PRODUCT'})
+        yield put({type: 'RDX_TOGGLE_SNACKBAR_OPEN', payload: {
+                isSnackBarDialogOpen: !action.payload.isSnackBarDialogOpen,
+                message: '上架了👍大成功😘'
+            }})
     } catch (error) {
-        yield put({type: 'RDX_TOGGLE_ERROR_DIALOG_OPEN'})   // open error dialog
         yield put({type: 'RDX_TOGGLE_PRODUCT_DIALOG_OPEN', payload: {
                     isAddProductDialogOpen: action.payload.isAddProductDialogOpen
                 }}) // close product dialog
+        yield put({type: 'RDX_TOGGLE_ERROR_DIALOG_OPEN'})   // open error dialog
     }
 }
 
