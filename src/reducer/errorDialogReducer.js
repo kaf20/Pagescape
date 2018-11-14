2682181ab368305db21123592c5552d87fb25da8
Lau Ka Fai <nelson.kf.lau@gmail.com>
2018-11-14 23:48:13 +0800

    Toggle error dialog

--- src/reducer/errorDialogReducer.js
+++ src/reducer/errorDialogReducer.js
@@ -4,9 +4,15 @@ export default function errorDialogReducer(state = {
 }, action) {
     switch (action.type) {
         case 'RDX_TOGGLE_ERROR_DIALOG_OPEN':
+            let open = true
+            if (!!action.payload)
+                open = action.payload.isErrorDialogOpen
+            let msg = '出事'
+            if ((!!action.payload && !!action.payload.errorMessage))
+                msg = action.payload.errorMessage
             return Object.assign({}, state, {
-                isErrorDialogOpen: action.payload.isErrorDialogOpen,
-                errorMessage: action.payload.errorMessage,
+                isErrorDialogOpen:  open,
+                errorMessage:  msg,
             })
         default:
             return state
