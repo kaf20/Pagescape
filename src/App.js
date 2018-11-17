ea11574cbb91437ef3e7b0588d0e44cbb01bb570
Lau Ka Fai <nelson.kf.lau@gmail.com>
2018-11-18 02:23:18 +0800

    added context menu

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import Snackbar from '@material-ui/core/Snackbar'
 import Fade from '@material-ui/core/Fade'
 import ErrorDialog from './component/common/ErrorDialog'
+import {ContextMenu, ContextMenuTrigger, MenuItem} from 'react-contextmenu'
+
 
 const styles = theme => ({
     snackBar: {
@@ -31,29 +33,50 @@ const App = (props) => {
 
     const dispatch = (type, payload) => store.dispatch({type, payload})
     const handleSnackbarClose = event => dispatch('RDX_TOGGLE_SNACKBAR_OPEN', {isSnackBarDialogOpen: !isSnackBarDialogOpen})
+    const handleClick = (e, data) => {
+        console.log(data.foo)
+    }
+    const handleHide = (event) => {
+        event.preventDefault()
+        return false
+    }
 
     return (
         <BrowserRouter>
             <div className='App'>
-                <CssBaseline />
-                <Header dispatch={dispatch} state={state}/>
-                <Switch>
-                    <Route exact path='/' render={() => (
-                        <Home dispatch={dispatch} state={state}/>
-                    )}/>
-                    <Route component={ErrorPage}/>
-                </Switch>
-                <ErrorDialog dispatch={dispatch} state={state}/>
-                <Snackbar
-                    anchorOrigin={{vertical: 'top', horizontal: 'right'}}
-                    open={isSnackBarDialogOpen}
-                    onClose={handleSnackbarClose}
-                    TransitionComponent={Fade}
-                    ContentProps={{
-                        'aria-describedby': 'message-id',
-                    }}
-                    message={<div className={classes.snackBar} id='message-id'>{message}</div>}
-                />
+                <ContextMenuTrigger id='contentMenuId-000'>
+                    <CssBaseline />
+                    <Header dispatch={dispatch} state={state}/>
+                    <Switch>
+                        <Route exact path='/' render={() => (
+                            <Home dispatch={dispatch} state={state}/>
+                        )}/>
+                        <Route component={ErrorPage}/>
+                    </Switch>
+                    <ErrorDialog dispatch={dispatch} state={state}/>
+                    <Snackbar
+                        anchorOrigin={{vertical: 'top', horizontal: 'right'}}
+                        open={isSnackBarDialogOpen}
+                        onClose={handleSnackbarClose}
+                        TransitionComponent={Fade}
+                        ContentProps={{
+                            'aria-describedby': 'message-id',
+                        }}
+                        message={<div className={classes.snackBar} id='message-id'>{message}</div>}
+                    />
+                </ContextMenuTrigger>
+                <ContextMenu id='contentMenuId-000' onHide={handleHide}>
+                    <MenuItem data={{like: 1}} onClick={handleClick}>
+                        <h1>👍 like</h1>
+                    </MenuItem>
+                    <MenuItem data={{appreciate: 1}} onClick={handleClick}>
+                        <h1>🙇🏻‍ appreciate</h1>
+                    </MenuItem>
+                    <MenuItem divider />
+                    <MenuItem data={{dislike: 1}} onClick={handleClick}>
+                        <h1>👎 dislike</h1>
+                    </MenuItem>
+                </ContextMenu>
             </div>
         </BrowserRouter>
     )
