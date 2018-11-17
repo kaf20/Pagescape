a2654d20c2781d505e2109bb8ce7fc0d95edf89a
Lau Ka Fai <nelson.kf.lau@gmail.com>
2018-11-18 01:23:18 +0800

    infinite scroll supported

--- src/reducer/retrieveProductReducer.js
+++ src/reducer/retrieveProductReducer.js
@@ -1,10 +1,21 @@
 export default function retrieveProductReducer(state = {
     products: [],
+    hasMore: false,
+    page: 0,
+    size: 6,
 }, action) {
     switch (action.type) {
+        case 'RDX_RETRIEVE_PRODUCT_NEXT_PAGE':
+            return Object.assign({}, state, {
+                page: action.payload.page
+            })
         case 'RDX_RETRIEVE_PRODUCT_SUCCESS':
             return Object.assign({}, state, {
-                products: action.payload
+                products: state.products.concat(action.payload.products)
+            })
+        case 'RDX_RETRIEVE_PRODUCT_LOAD_MORE':
+            return Object.assign({}, state, {
+                hasMore: action.payload.hasMore
             })
         case 'RDX_RETRIEVE_PRODUCT_FAIL':
         default:
--- src/sagas/retrieveProductSaga.js
+++ src/sagas/retrieveProductSaga.js
@@ -3,10 +3,24 @@ import axios from 'axios'
 
 function* doRetrieve(action) {
     try {
+        const payload = action.payload || {}
         const api = () =>
-            axios.get('/product')
+            axios.get('/product', {
+                params: {
+                    page: payload.page || 0,
+                    size: payload.size || 6,
+                }
+            })
         const result = yield call(api)
-        yield put({type: 'RDX_RETRIEVE_PRODUCT_SUCCESS', payload: result.data})
+        yield put({type: 'RDX_RETRIEVE_PRODUCT_SUCCESS', payload: {
+            products: result.data.content
+        }})
+        yield put({type: 'RDX_RETRIEVE_PRODUCT_LOAD_MORE', payload: {
+            hasMore: !result.data.last
+        }})
+        yield put({type: 'RDX_RETRIEVE_PRODUCT_NEXT_PAGE', payload: {
+            page: payload.page + 1
+        }})
     } catch (error) {
         yield put({type: 'RDX_RETRIEVE_PRODUCT_FAIL'})
     }
