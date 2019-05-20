8e69bc0a8c4a36d7b9b2ca0bb6e1e9c4540040b0
kaf20 <nelson.kf.lau@gmail.com>
2019-05-21 00:15:33 +0800

    VBN-0001: Vue era

--- src/AxiosConfig.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-let domain = 'http://localhost'
-if (process.env.NODE_ENV === 'production')
-    domain = 'https://pric.me'
-axios.defaults.baseURL = `${domain}/vibranium/api/v1`
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN
-axios.defaults.headers.post['Content-Type'] = 'application/json'
