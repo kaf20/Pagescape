b08e0dd22131e6a256034bb281c6060be3f31399
kaf20 <nelson.kf.lau@gmail.com>
2019-08-03 22:46:27 +0800

    PAGESCAPE-002: To double quote

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
-import Vue from 'vue'
+import Vue from "vue";
 import VueRouter from "vue-router";
+import VueMaterial from "vue-material";
 import Privacy from "./components/Privacy";
 import App from "./App";
 import PostPanel from "./components/PostPanel";
+import "vue-material/dist/vue-material.min.css";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
+Vue.use(VueMaterial);
+
 const routes = [
-    { path: '/', component: App },
-    { path: '/privacy', component: Privacy },
-    { path: '/posts', component: PostPanel },
+    { path: "/", component: App },
+    { path: "/privacy", component: Privacy },
+    { path: "/posts", component: PostPanel },
 ];
 
 const router = new VueRouter({
-    mode: 'history',
+    mode: "history",
     base: __dirname,
     routes
 });
@@ -21,4 +25,4 @@ const router = new VueRouter({
 new Vue({
     router,
     template: `<router-view class="view"></router-view>`
-}).$mount('#app');
+}).$mount("#app");
